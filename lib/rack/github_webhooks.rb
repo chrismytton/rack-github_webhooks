36a22e22487a6e3d64383b0118f3a441769cd9b4
Chris Mytton <chrismytton@gmail.com>
2015-11-29 15:13:29 +0000

    Refactor to have separate Signature class

--- lib/rack/github_webhooks.rb
+++ lib/rack/github_webhooks.rb
@@ -4,11 +4,22 @@ require 'json'
 
 module Rack
   class GithubWebhooks
-    HMAC_DIGEST = OpenSSL::Digest.new('sha1')
+    class Signature
+      HMAC_DIGEST = OpenSSL::Digest.new('sha1')
 
-    attr_reader :app
-    attr_reader :secret
-    attr_reader :request
+      def initialize(secret, hub_signature, payload_body)
+        @secret = secret
+        @hub_signature = hub_signature
+        @signature = 'sha1=' +
+                     OpenSSL::HMAC.hexdigest(HMAC_DIGEST, secret, payload_body)
+      end
+
+      def valid?
+        return true unless @secret
+        return false unless @hub_signature
+        Rack::Utils.secure_compare(@signature, @hub_signature)
+      end
+    end
 
     def initialize(app, opts = {})
       @app = app
@@ -16,29 +27,14 @@ module Rack
     end
 
     def call(env)
-      @request = Rack::Request.new(env)
-      return [400, {}, ["Signatures didn't match!"]] unless signature_valid?
-      app.call(env)
-    end
-
-    private
-
-    # Taken from https://developer.github.com/webhooks/securing/
-    def signature_valid?
-      return true unless secret
-      return false unless request.env['HTTP_X_HUB_SIGNATURE']
-      Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
-    end
-
-    def signature
-      "sha1=#{OpenSSL::HMAC.hexdigest(HMAC_DIGEST, secret, payload_body)}"
-    end
-
-    def payload_body
-      @payload_body ||= begin
-        request.body.rewind
-        request.body.read
-      end
+      env['rack.input'].rewind
+      signature = Signature.new(
+        @secret,
+        env['HTTP_X_HUB_SIGNATURE'],
+        env['rack.input'].read
+      )
+      return [400, {}, ["Signatures didn't match!"]] unless signature.valid?
+      @app.call(env)
     end
   end
 end
